d49c4cedc28c13a8683c25e8498d509b8098217e
Warren Buckley <warren@hackmakedo.com>
2024-06-06 08:22:09 +0000

    Umbraco Project Scaffolded & Vite setup added

---
+++ testing-template.client/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/index.ts", // Entrypoint file (registers other manifests)
+      formats: ["es"],
+      fileName: 'testing-template',
+    },
+    outDir: '..//home/runner/work/Testing-Template/Testing-Template/Testing-Template/wwwroot', // your web component will be saved to the RCL project location and the RCL sets the path as App_Plugins/Testing-Template
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      external: [/^@umbraco/],
+    },
+  },
+});
